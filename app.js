3077c4126d10986884dc4973ae46082894886fe2
Peter.O <ojakaapeter@gmail.com>
2017-06-26 17:23:49 +0300

    Create appropriate messages for the get started

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const bot = new BootBot({
 });
 
 bot.setGetStartedButton((payload, chat) => {
-  const welcome1 = `Hey there, trainer! How well you think you ...`;
-  const welcome2 = `Type START or PLAY to join the challenge!`;
+  const welcome = `Hey there, I can help you order cakes and doughnuts just from your phone, and facebook messenger!`;
   const options = { typing: true };
 
-  chat.say(welcome1, options)
-    .then(() => chat.say(welcome2, options));
+  // Greet the customer then ask what he would like to order, a cake or doughnut?
+  chat.say(welcome, options)
+    .then(() => chat.say({
+		text: 'What would you like to order today?',
+		quickReplies: ['Cake', 'Doughnut']
+	}));
 });
 
 bot.start();
