71bf7b3295c1604eda865e149d0cb6ae61e25b33
Peter.O <ojakaapeter@gmail.com>
2017-06-26 19:42:46 +0300

    List for cake complete

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ bot.setGetStartedButton((payload, chat) => {
   chat.say(welcome, options)
     .then(() => chat.say({
 		text: 'What would you like to order today?',
-		quickReplies: ['Cake', 'Doughnut']
+		quickReplies: ['Cake','Doughnut']
 	}));
 });
 
@@ -26,72 +26,119 @@ bot.on('message', (payload, chat) => {
 
   const options = { typing: true };
 
-  chat.sendListTemplate([{
-      "title": "Classic T-Shirt Collection",
-      "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-      "subtitle": "See all our colors",
-      "buttons": [
-          {
-              "title": "View",
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/collection",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-          }
-      ]
-  },
-  {
-      "title": "Classic White T-Shirt",
-      "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
-      "subtitle": "100% Cotton, 200% Comfortable",
-      "buttons": [
-          {
-              "title": "Shop Now",
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-          }
-      ]
-  },
-  {
-      "title": "Classic Blue T-Shirt",
-      "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-      "subtitle": "100% Cotton, 200% Comfortable",
-      "buttons": [
-          {
-              "title": "Shop Now",
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-          }
-      ]
-  },
-  {
-      "title": "Classic Black T-Shirt",
-      "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
-      "subtitle": "100% Cotton, 200% Comfortable",
-      "buttons": [
-          {
-              "title": "Shop Now",
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-          }
-      ]
-  }], [
-     {
-         "title": "View More",
-         "type": "postback",
-         "payload": "payload"
-     }
- ], options);
+  if(text  === 'Cake'){
+    chat.sendListTemplate([{
+        "title": "Black forest",
+        "image_url": "https://d2z4fd79oscvvx.cloudfront.net/0023640_black_forest_cake.jpeg",
+        "subtitle": "Consists of several layers of chocolate sponge cake sandwiched with whipped cream and cherries.",
+        "buttons": [
+            {
+              "title": "Buy now",
+              "type": "postback",
+              "payload": "BLACKFOREST_PAYLOAD"
+            }
+        ]
+    },
+    {
+        "title": "White forest",
+        "image_url": "http://trivandrumcakehouse.com/wp-content/uploads/2015/10/cake-white-forest.jpg",
+        "subtitle": "This impressive spin on a Black Forest cake uses sweet white chocolate instead of dark chocolate.",
+        "buttons": [
+            {
+              "title": "Buy now",
+              "type": "postback",
+              "payload": "WHITEFOREST_PAYLOAD"
+            }
+        ]
+    },
+    {
+        "title": "Sponge cake",
+        "image_url": "http://img.taste.com.au/NKPhrv0q/taste/2016/11/foolproof-sponge-102144-1.jpeg",
+        "subtitle": "Sponge cake is a cake based on flour, sugar, butter and eggs, and is sometimes leavened with baking powder. ",
+        "buttons": [
+            {
+              "title": "Buy now",
+              "type": "postback",
+              "payload": "SPONGECAKE_PAYLOAD"
+            }
+        ]
+    },
+    {
+        "title": "Frog cake",
+        "image_url": "http://www.eatlivetravelwrite.com/wp-content/uploads/2016/01/Homemade-frog-cake-on-eatlivetravelwrite.com_.jpg",
+        "subtitle": "The frog cake is a dessert in the shape of a frog's head, composed of sponge cake and cream covered with fondant.",
+        "buttons": [
+            {
+              "title": "Buy now",
+              "type": "postback",
+              "payload": "FROGCAKE_PAYLOAD"
+            }
+        ]
+    }], [
+       {
+           "title": "View More",
+           "type": "postback",
+           "payload": "VIEWMORE_PAYLOAD"
+       }
+   ], options);
+ } else if (text === 'Doughnut'){
+   chat.sendListTemplate([{
+       "title": "Black forest",
+       "image_url": "https://d2z4fd79oscvvx.cloudfront.net/0023640_black_forest_cake.jpeg",
+       "subtitle": "Consists of several layers of chocolate sponge cake sandwiched with whipped cream and cherries.",
+       "buttons": [
+           {
+             "title": "Pen",
+             "type": "postback",
+             "payload": "BLACKFOREST_PAYLOAD"
+           }
+       ]
+   },
+   {
+       "title": "White forest",
+       "image_url": "http://trivandrumcakehouse.com/wp-content/uploads/2015/10/cake-white-forest.jpg",
+       "subtitle": "This impressive spin on a Black Forest cake uses sweet white chocolate instead of dark chocolate.",
+       "buttons": [
+           {
+             "title": "Buy now",
+             "type": "postback",
+             "payload": "WHITEFOREST_PAYLOAD"
+           }
+       ]
+   },
+   {
+       "title": "Sponge cake",
+       "image_url": "http://img.taste.com.au/NKPhrv0q/taste/2016/11/foolproof-sponge-102144-1.jpeg",
+       "subtitle": "Sponge cake is a cake based on flour, sugar, butter and eggs, and is sometimes leavened with baking powder. ",
+       "buttons": [
+           {
+             "title": "Buy now",
+             "type": "postback",
+             "payload": "SPONGECAKE_PAYLOAD"
+           }
+       ]
+   },
+   {
+       "title": "Frog cake",
+       "image_url": "http://www.eatlivetravelwrite.com/wp-content/uploads/2016/01/Homemade-frog-cake-on-eatlivetravelwrite.com_.jpg",
+       "subtitle": "The frog cake is a dessert in the shape of a frog's head, composed of sponge cake and cream covered with fondant.",
+       "buttons": [
+           {
+             "title": "Buy now",
+             "type": "postback",
+             "payload": "FROGCAKE_PAYLOAD"
+           }
+       ]
+   }], [
+      {
+          "title": "View More",
+          "type": "postback",
+          "payload": "VIEWMORE_PAYLOAD"
+      }
+  ], options);
+ }
+
+
 
 });
 
